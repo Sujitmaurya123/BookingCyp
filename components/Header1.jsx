8515651558fd2ef9c324ca3118657dfb154e56b4
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2024-06-20 12:17:31 +0530

    Login/Signup Create with backend Api done

--- components/Header1.jsx
+++ components/Header1.jsx
@@ -1,10 +1,24 @@
 import Image from "next/image"
 import Block from "./Block"
 import Link from "next/link"
-
+import Cookies from "js-cookie"
+import { useRouter } from "next/router"
 
 
 const Header1 = () => {
+
+
+  let auth;
+  if(typeof window !=='undefined'){
+
+     auth =Cookies.get("user");
+  }
+const router=useRouter();
+  const handleLogout=()=>{
+    Cookies.remove('user');
+    router.push("/");
+  }
+
   return (
     <div className="flex justify-between border-b-2 border-gray-300 items-center h-24 px-10">
         <Image src={'/logobooking.png'} alt="" width={200} height={200} className="w-24 h-24"></Image>
@@ -15,9 +29,16 @@ const Header1 = () => {
          <Block  title={'987654321'} para={'Call us to book now.'} />
          <div className="flex items-center">
        
-            <Link href={'/login'}>
+           {
+            auth ?(
+                
+              <h3 className="font-bold  cursor-pointer" onClick={handleLogout} >Logout</h3>
+          
+            ):
+            ( <Link href={'/login'}>
               <h3 className="font-bold">Login/Signup</h3>
-            </Link>
+            </Link>)
+           }
          </div>
        </div>
     </div>
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,7 +1,40 @@
-import Head from "next/head"
+'use client';
 
+import axios from "axios";
+import Head from "next/head"
+import { useState } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
 
 const Login = () => {
+    const [name,setName]=useState("");
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const [login ,setLogin]=useState(false);
+
+    const router=useRouter();
+
+    const handleSignup=async()=>{
+         const res= await axios.post(`api/user/register`,{name,email,password});
+        if(res?.data){
+            Cookies.set('user',res.data.token);
+          alert(res.data.msg);
+          router.push('/');
+
+        }
+    }
+    const handleLogin=async()=>{
+        const res= await axios.post(`api/user/login`,{email,password});
+        if(res?.data){
+            Cookies.set('user',res.data.token);
+          alert(res.data.msg);
+          router.push('/');
+        }
+    }
+
+     const handleToggle=()=>{
+        setLogin(!login);
+    }
   return (
     <div>
         <Head>
@@ -25,14 +58,19 @@ const Login = () => {
              <div className="px-10 ">
                 <h3 className="text-5xl font-bold my-5">Login/Signup</h3>
                 <p className="font-bold text-lg mb-1">Please enter your phone number to continue</p>
-                <input type="name" placeholder="Enter your name.." className="outline-none border border-black px-3 py-1 w-96 h-10 " />
-                <input type="email" placeholder="Enter your email.." className="outline-none border border-black px-3 py-1 w-96 h-10 mt-4" />
-                <input type="password" placeholder="Enter your password.." className="outline-none border border-black px-3 py-1 w-96 h-10 mt-4" />
+                {
+                    login ?(""):(
+                <input type="name" placeholder="Enter your name.." className="outline-none border border-black px-3 py-1 w-96 h-10 "  onChange={(e)=>setName(e.target.value)} />
+
+                    )
+                }
+                <input type="email" placeholder="Enter your email.." className="outline-none border border-black px-3 py-1 w-96 h-10 mt-4"  onChange={(e)=>setEmail(e.target.value)} />
+                <input type="password" placeholder="Enter your password.." className="outline-none border border-black px-3 py-1 w-96 h-10 mt-4"  onChange={(e)=>setPassword(e.target.value)} />
                 
-                <button type="submit" className=" w-96 h-14 text-lg font-bold bg-red-400 hover:cursor-pointer hover:bg-red-600 text-white my-5 rounded-md">Sign Up</button>
+                <button type="submit" className=" w-96 h-14 text-lg font-bold bg-red-400 hover:cursor-pointer hover:bg-red-600 text-white my-5 rounded-md" onClick={ login? handleLogin: handleSignup} >{login?"Login":"Sign Up"}</button>
                 <p className="my-1 text-xl ">
-                    <span>Already have an account ?</span>
-                    <span className="ml-1 border-b-2 border-red-500 text-red-600 hover:cursor-pointer">Login</span>
+                    <span>{login? "Don`t have an account":"Already have an account ?"}</span>
+                    <span className="ml-1 border-b-2 border-red-500 text-red-600 hover:cursor-pointer" onClick={handleToggle} >{""}{login?"Sign Up":"Login"}</span>
                 </p>
              </div>
             </div>
